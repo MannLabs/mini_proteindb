af76c1c5adf272fc1b0c566a540973fadb0edcf1
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-25 14:55:00 +0100

    added unit test to check that db contains protein table

--- proteindb/proteindb.py
+++ proteindb/proteindb.py
@@ -1,14 +1,30 @@
 import sqlite3
 
 
+def setup_db(filename):
+    conn = sqlite3.connect(filename)
+    c = conn.cursor()
+    try:
+        c.execute('''CREATE TABLE proteins
+                             (id INTEGER, uniprot_id TEXT, sequence TEXT)''')
+        c.execute("PRAGMA table_info('proteins');")
+    except sqlite3.OperationalError as e:
+        if 'already exists' in str(e):
+            pass
+        else:
+            raise e
+    conn.commit()
+    return conn
+
+
 class ProteinDB:
     def __init__(self, filename='protein.db'):
-        self.db = sqlite3.connect(filename)
+        self._conn = setup_db(filename)
         self.result = None
 
     def populate(self, filename):
         pass
 
     def query(self, query_string):
-        self.db.query(query_string)
-        self.result = self.db.get_result()
+        self._conn.query(query_string)
+        self.result = self._conn.get_result()
--- tests/test_database.py
+++ tests/test_database.py
@@ -3,4 +3,17 @@ import sqlite3
 
 def test_proteindb_contains_database():
     db = ProteinDB('test.db')
-    assert isinstance(db.db, sqlite3.Connection)
+    assert isinstance(db._conn, sqlite3.Connection)
+
+
+def test_db_contains_protein_table():
+    db = ProteinDB('test.db')
+    conn = db._conn
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    results = c.fetchall()
+    assert len(results) == 1
+    assert results[0][0] == 'proteins'
+
+# def test_
+#     c.execute(".tables")
