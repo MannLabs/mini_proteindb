517128661412be14597cdfa245e18a43b5dbc59b
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-25 15:32:32 +0100

    refactored tests to use fixtures

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,25 +1,28 @@
 from proteindb import ProteinDB
 import sqlite3
+import pytest
 
-def test_proteindb_contains_database():
-    db = ProteinDB('test.db')
+@pytest.fixture
+def db():
+    return ProteinDB('test.db')
+
+@pytest.fixture
+def cursor(db):
+    conn = db._conn
+    return conn.cursor()
+
+def test_proteindb_contains_database(db):
     assert isinstance(db._conn, sqlite3.Connection)
 
 
-def test_db_contains_protein_table():
-    db = ProteinDB('test.db')
-    conn = db._conn
-    c = conn.cursor()
-    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
-    results = c.fetchall()
+def test_db_contains_protein_table(cursor):
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    results = cursor.fetchall()
     assert len(results) == 1
     assert results[0][0] == 'proteins'
 
-def test_db_table_contains_all_columns():
-    db = ProteinDB('test.db')
-    conn = db._conn
-    c = conn.cursor()
-    c.execute("PRAGMA table_info('proteins');")
-    results = c.fetchall()
+def test_db_table_contains_all_columns(cursor):
+    cursor.execute("PRAGMA table_info('proteins');")
+    results = cursor.fetchall()
     columns = [result[1] for result in results]
     assert 'uniprot_id' in columns and 'sequence' in columns
