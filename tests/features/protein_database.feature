10ec8c3e13e18590d448c233b4e132cd33385915
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2019-01-25 13:10:35 +0100

    Creation of ProteinDB class, added and moved examples table, replaced population scenario with a Background, added fields to example table

--- tests/features/protein_database.feature
+++ tests/features/protein_database.feature
@@ -1,22 +1,20 @@
 Feature: Protein Database
 
-  Scenario Outline: Populating the Database
-    Given I have a Fasta file named <filename> that contains protein sequences
-    When I populate a protein database with the Fasta files
-    Then all the sequences from the Fasta files are inside the database
+  Examples: Vertical
+    | filename   | uni6283.fasta |
+    | uniprot_id | dsafnadf1     |
+    | sequence   | ADNFADFHJ     |
 
-    Examples: Vertical
-    | filename | uni6283.fasta |
+  Background:
+    Given I have a database populated with data from Fasta file named <filename>.
 
   Scenario: Sequence Search by ID
-    Given the protein database
-    And and a Uniprot ID
-    When I search for ID in a database
-    Then I will see the sequence associated with the ID
+    Given <uniprot_id>
+    When I search for <uniprot_id> in a database
+    Then I will see the corresponding <sequence>
 
   Scenario: ID Search by Sequence
-    Given a sequence
-    And and the protein database
-    When I search for the sequence in the database
-    Then I will see all IDs associated with the sequence
+    Given <sequence>
+    When I search for the <sequence> in the database
+    Then I will see the corresponding <uniprot_id>
 
--- tests/test_protein_database.py
+++ tests/test_protein_database.py
@@ -1,27 +1,27 @@
 import pytest
 from pytest_bdd import scenario, given, when, then, scenarios
 
-@pytest.fixture
-def db():
+
+class ProteinDB:
+    def __init__(self):
+        self.db = None
+        self.result = None
+
+    def populate(self, filename):
+        pass
+
+    def search_id(self, uniprot_id):
+        self.result = None
+
+
+
+
+@given("I have a database populated with data from Fasta file named <filename>.")
+def db(filename):
+
+    db = ProteinDB()
+    db.populate(filename)
     return db
-
-
-######
-@scenario('protein_database.feature', 'Populating the Database')
-def test_foo():
-    pass
-
-@given("I have a Fasta file named <filename> that contains protein sequences", target_fixture=db)
-def fasta(filename):
-    raise NotImplemented
-
-@when("I populate a protein database with the Fasta files")
-def build_and_populate_database(fasta, db):
-    raise NotImplemented
-
-@then("all the sequences from the Fasta files are inside the database")
-def database_contains_all_sequences(db, fasta):
-    raise NotImplemented
 
 
 #####
@@ -29,44 +29,39 @@ def database_contains_all_sequences(db, fasta):
 def test_search_by_ID():
     pass
 
-@given("the protein database")
-def step_impl(db):
-    return db
+
+@given("<uniprot_id>")
+def uniprot_id(uniprot_id):
+    return uniprot_id
 
 
-@given("and a Uniprot ID")
-def step_impl():
-    raise NotImplementedError(u'STEP: And and a Uniprot ID')
+@when("I search for <uniprot_id> in a database")
+def step_impl(db, uniprot_id):
+    db.search_id(uniprot_id)
 
 
-@when("I search for ID in a database")
-def step_impl():
-    raise NotImplementedError(u'STEP: When I search for ID in a database')
+@then("I will see the corresponding <sequence>")
+def step_impl(db, sequence):
+    assert db.result == sequence
 
 
-@then("I will see the sequence associated with the ID")
-def step_impl():
-    raise NotImplementedError(u'STEP: Then I will see the sequence associated with the ID')
-
+#############
 @scenario('protein_database.feature', "ID Search by Sequence")
 def test_search_by_ID():
     pass
 
-@given("a sequence")
-def step_impl():
-    raise NotImplementedError(u'STEP: Given a sequence')
+@given("<sequence>")
+def sequence(sequence):
+    return sequence
 
 
-@given("and the protein database")
-def step_impl():
-    raise NotImplementedError(u'STEP: And and the protein database')
+
+@when("I search for the <sequence> in the database")
+def step_impl(db, sequence):
+    db.search_sequence(sequence)
 
 
-@when("I search for the sequence in the database")
-def step_impl():
-    raise NotImplementedError(u'STEP: When I search for the sequence in the database')
+@then("I will see the corresponding <uniprot_id>")
+def step_impl(db, uniprot_id):
+    assert db.result == uniprot_id
 
-
-@then("I will see all IDs associated with the sequence")
-def step_impl():
-    raise NotImplementedError(u'STEP: Then I will see all IDs associated with the sequence')
